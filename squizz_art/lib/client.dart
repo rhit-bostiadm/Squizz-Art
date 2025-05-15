0c585378c6c78689d558b0f4f12f528e105d20d4
rhit-fieldms <fieldms@rose-hulman.edu>
2025-05-15 01:35:43 -0400

    Trying to get stuff working.

---
+++ squizz_art/lib/client.dart
@@ -0,0 +1,83 @@
+import 'dart:async';
+import 'package:dart_ipify/dart_ipify.dart';
+import 'package:squizz_art/drawing.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class Client {
+  late String ip;
+  final port = 80;
+
+  Client() {
+    ip = "137.112.216.124";
+    print("IP: $ip");
+  }
+
+  // Future<void> setupClient() async {
+  //   // ip = await Ipify.ipv4();
+    
+    
+  //   // final request = await client.getUrl(Uri.parse('http://$ip'));
+  //   // await request.close();
+  //   // client.listen((HttpRequest request) {
+  //   //   handleRequest(request);
+  //   // });
+  // }
+
+  // void handleRequest(HttpRequest request) {
+  //   // HttpResponse response;
+  //   // switch (request.method) {
+  //   //   case 'GET':
+  //       // response.headers
+  //       request.response.write("This is a test for the Squizz App HTTP server");
+  //       request.response.close();
+  //   //     break;
+  //   // }
+  // }
+
+  Future<Drawing> sendDrawing(Drawing drawing) async {
+    final response = await http.post(
+      Uri.parse('http://$ip'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+        "Accept": "application/json",
+      },
+      body: jsonEncode(drawing.toJson()),
+    );
+
+    print("Oh, ok then.\n");
+
+    if (response.statusCode == 201) {
+      return Drawing.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+    } else {
+      throw Exception('Failed to send drawing.');
+    }
+  }
+
+  Future<void> getNewCanvas(List<Drawing> drawings) async {
+    final response = await http.get(
+      Uri.parse('http://$ip'),
+    );
+
+    print("Erm, what the squizz\n");
+
+    if (response.statusCode == 200) {
+      if (drawings.isEmpty) {
+        return await getNewCanvas(drawings);
+      }
+
+      Drawing drawing = Drawing.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+      drawings.add(drawing);
+      return await getNewCanvas(drawings);
+    } else if (response.statusCode == 502) {
+      return await getNewCanvas(drawings);
+    } else {
+      throw Exception('HTTP GET request failed.');
+    }
+  }
+}
+
+
+
+
+
--- squizz_art/lib/drawing.dart
+++ squizz_art/lib/drawing.dart
@@ -7,4 +7,26 @@ class Drawing {
   final String tool;
 
   Drawing({required this.points, this.color = Colors.black, this.size = 10, this.tool = "pencil"});
+
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>> pointsMap =
+        points.map((offset) => {'dx': offset.dx, 'dy': offset.dy}).toList();
+    return {
+      'points': pointsMap,
+      'color': color.value.toRadixString(16),
+      'size': size,
+      'tool': tool
+    };
+  }
+
+  factory Drawing.fromJson(Map<String, dynamic> json) {
+    List<Offset> points =
+        (json['points'] as List).map((e) => Offset(double.parse(e['dx'].toString()), double.parse(e['dy'].toString()))).toList();
+    return Drawing(
+      points: points,
+      color: Color(int.parse((json['color'] as String), radix: 16)),
+      size: double.parse(json['size'].toString()),
+      tool: "pencil" // change this
+    );
+  }
 }
